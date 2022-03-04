1afc83c264070089a67f38db7d11e775880ab913
Alejandro Luna <alunag95@gmail.com>
2022-03-04 11:11:02 -0600

    added new files

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  # get 'lists/index'
+  # get 'lists/show'
+  # get 'lists/new'
+  # get 'lists/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'lists#index'
+  resources :lists, only: [:index]
 end
